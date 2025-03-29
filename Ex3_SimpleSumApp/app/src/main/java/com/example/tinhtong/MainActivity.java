34fd979014897fd53cbf7517364dfa15b1f330bb
nguyendinhnguyen1 <thanzbong115@gmail.com>
2025-03-29 22:47:57 +0700

    làm lại

--- Ex3_SimpleSumApp/app/src/main/java/com/example/tinhtong/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/com/example/tinhtong/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.tinhtong;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.edtA), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+    public void xulyCong(View view){
+        EditText editTextA = findViewById(R.id.edtA);
+        EditText editTextB = findViewById(R.id.edtB);
+        EditText editTextKetqua = findViewById(R.id.edtKq);
+
+       String strA = editTextA.getText().toString();
+       String strB = editTextB.getText().toString();
+
+       int soA = Integer.parseInt(strA);
+       int soB = Integer.parseInt(strB);
+
+       int tong = soA + soB;
+       String strTong = String.valueOf(tong);
+
+       editTextKetqua.setText(strTong);
     }
 }
